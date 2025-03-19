22529ba9f1dcb78f1e4ba1898a73c1ad4ed37108
aswinthulasir <aswinthulasi007@gmail.com>
2025-03-19 16:12:32 +0530

    reverse-caching

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-import psycopg2
-from app.config import settings
 
-app = FastAPI() 
+app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-try:
-    conn = psycopg2.connect(
-        host=settings.database_hostname,
-        port=settings.database_port,
-        dbname=settings.database_name,
-        user=settings.database_username,
-        password=settings.database_password
-    )
-    print("Database connected successfully!")
-    print(settings.database_hostname)
-    print(settings.database_port)
-    print(settings.database_name)
-    conn.close()
-except Exception as e:
-    print(f"Database connection failed: {e}")
+    return {""}
